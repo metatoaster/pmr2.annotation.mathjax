9ff08f0b76e02a90ca8bc155f57309156c83cfe3
Tommy Yu <tommy.yu@auckland.ac.nz>
2012-09-05 20:25:16 +1200

    Removal of layout wrappers.
    Progress towards: tracker item 3386

--- pmr2/annotation/mathjax/browser.py
+++ pmr2/annotation/mathjax/browser.py
@@ -1,13 +1,9 @@
 import zope.component
 from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
 from zope.publisher.browser import BrowserPage
-from plone.z3cform import layout
-from z3c.form.interfaces import IForm
 
 from pmr2.app.exposure.interfaces import IExposureSourceAdapter
 from pmr2.app.exposure.browser.browser import ExposureFileViewBase
-
-from pmr2.annotation.mathjax.layout import MathJaxLayoutWrapper
 
 
 class MathJaxNote(ExposureFileViewBase):
